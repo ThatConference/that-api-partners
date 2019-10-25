028c662434e9d5cfd90f681016b0448339e9f461
Clark Sell <clark.sell@csell.net>
2019-10-25 15:01:33 -0500

    updating resolver

--- src/graphql/resolvers/queries/partners.js
+++ src/graphql/resolvers/queries/partners.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid/v4';
 
 const resolvers = {
-  echo: async (parent, { message }, { dataSources }) => ({
+  partners: async (parent, { message }, { dataSources }) => ({
     id: uuid(),
     message: `resolver got: ${message}`,
   }),
