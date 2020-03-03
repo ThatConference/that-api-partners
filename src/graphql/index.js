9ef3f1ffded142e29e1d39031e6e0a6942464b4f
Clark Sell <clark.sell@csell.net>
2020-03-03 07:31:45 -0600

    reworks sentry a bit for better logging

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -9,6 +9,7 @@ import debug from 'debug';
 import { security, graph } from '@thatconference/api';
 import _ from 'lodash';
 import DataLoader from 'dataloader';
+import * as Sentry from '@sentry/node';
 
 // Graph Types and Resolvers
 import typeDefsRaw from './typeDefs';
@@ -36,7 +37,6 @@ const typeDefs = gql`
  */
 const createServer = ({ dataSources }, enableMocking = false) => {
   let federatedSchemas = {};
-  const { logger } = dataSources;
 
   if (!enableMocking) {
     federatedSchemas = buildFederatedSchema([{ typeDefs, resolvers }]);
@@ -87,10 +87,22 @@ const createServer = ({ dataSources }, enableMocking = false) => {
 
       if (!_.isNil(req.headers.authorization)) {
         dlog('validating token for %o:', req.headers.authorization);
+        Sentry.addBreadcrumb({
+          category: 'graphql context',
+          message: 'user has authToken',
+          level: Sentry.Severity.Info,
+        });
 
         const validatedToken = await jwtClient.verify(
           req.headers.authorization,
         );
+
+        Sentry.configureScope(scope => {
+          scope.setUser({
+            id: validatedToken.sub,
+            permissions: validatedToken.permissions.toString(),
+          });
+        });
 
         dlog('validated token: %o', validatedToken);
         context = {
@@ -118,9 +130,17 @@ const createServer = ({ dataSources }, enableMocking = false) => {
     ],
 
     formatError: err => {
-      logger.warn('graphql error', err);
+      dlog('format error %O', err);
 
-      dataSources.sentry.captureException(err);
+      Sentry.withScope(scope => {
+        scope.setTag('formatError', true);
+        scope.setLevel('warning');
+
+        scope.setExtra('originalError', err.originalError);
+        scope.setExtra('path', err.path);
+
+        Sentry.captureException(err);
+      });
 
       return err;
     },
